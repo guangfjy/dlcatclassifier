89f6fda54ad72266943fcc0e10063e1b9f13f1c4
guangfjy <guangfjy@gmail.com>
2021-11-30 16:28:47 +0800

    add a test jupyter file

--- test_utils.py
+++ test_utils.py
@@ -1,62 +1,64 @@
 import numpy as np
 
+
 def datatype_check(expected_output, target_output, error):
     success = 0
     if isinstance(target_output, dict):
         for key in expected_output.keys():
             try:
-                success += datatype_check(expected_output[key], 
-                                         target_output[key], error)
+                success += datatype_check(expected_output[key],
+                                          target_output[key], error)
             except:
                 print("Error: {} in variable {}. Got {} but expected type {}".format(error,
-                                                                          key, type(target_output[key]), type(expected_output[key])))
+                                                                                     key, type(target_output[key]), type(expected_output[key])))
         if success == len(target_output.keys()):
             return 1
         else:
             return 0
     elif isinstance(target_output, tuple) or isinstance(target_output, list):
         for i in range(len(expected_output)):
-            try: 
-                success += datatype_check(expected_output[i], 
-                                         target_output[i], error)
+            try:
+                success += datatype_check(expected_output[i],
+                                          target_output[i], error)
             except:
                 print("Error: {} in variable {}. Got type: {}  but expected type {}".format(error,
-                                                                          i, type(target_output[i]), type(expected_output[i])))
+                                                                                            i, type(target_output[i]), type(expected_output[i])))
         if success == len(target_output):
             return 1
         else:
             return 0
-                
+
     else:
         assert isinstance(target_output, type(expected_output))
         return 1
-            
+
+
 def equation_output_check(expected_output, target_output, error):
     success = 0
     if isinstance(expected_output, dict):
         for key in expected_output.keys():
             try:
-                success += equation_output_check(expected_output[key], 
-                                         target_output[key], error)
+                success += equation_output_check(expected_output[key],
+                                                 target_output[key], error)
             except:
                 print("Error: {} for variable {}.".format(error,
-                                                                          key))
+                                                          key))
         if success == len(target_output.keys()):
             return 1
         else:
             return 0
     elif isinstance(expected_output, tuple) or isinstance(expected_output, list):
         for i in range(len(expected_output)):
-            try: 
-                success += equation_output_check(expected_output[i], 
-                                         target_output[i], error)
+            try:
+                success += equation_output_check(expected_output[i],
+                                                 target_output[i], error)
             except:
                 print("Error: {} for variable {}.".format(error, i))
         if success == len(target_output):
             return 1
         else:
             return 0
-                
+
     else:
         if hasattr(expected_output, 'shape'):
             #np.allclose(target_output, expected_output)
@@ -64,14 +66,15 @@ def equation_output_check(expected_output, target_output, error):
         else:
             assert target_output == expected_output
         return 1
-    
+
+
 def shape_check(expected_output, target_output, error):
     success = 0
     if isinstance(expected_output, dict):
         for key in expected_output.keys():
             try:
-                success += shape_check(expected_output[key], 
-                                         target_output[key], error)
+                success += shape_check(expected_output[key],
+                                       target_output[key], error)
             except:
                 print("Error: {} for variable {}.".format(error, key))
         if success == len(expected_output.keys()):
@@ -80,21 +83,22 @@ def shape_check(expected_output, target_output, error):
             return 0
     elif isinstance(expected_output, tuple) or isinstance(expected_output, list):
         for i in range(len(expected_output)):
-            try: 
-                success += shape_check(expected_output[i], 
-                                         target_output[i], error)
+            try:
+                success += shape_check(expected_output[i],
+                                       target_output[i], error)
             except:
                 print("Error: {} for variable {}.".format(error, i))
         if success == len(expected_output):
             return 1
         else:
             return 0
-                
+
     else:
         if hasattr(expected_output, 'shape'):
             assert target_output.shape == expected_output.shape
         return 1
-                
+
+
 def single_test(test_cases, target):
     success = 0
     for test_case in test_cases:
@@ -112,19 +116,20 @@ def single_test(test_cases, target):
                 success += 1
         except:
             print("Error: " + test_case['error'])
-            
+
     if success == len(test_cases):
         print("\033[92m All tests passed.")
     else:
-        print('\033[92m', success," Tests passed")
+        print('\033[92m', success, " Tests passed")
         print('\033[91m', len(test_cases) - success, " Tests failed")
         raise AssertionError("Not all tests were passed for {}. Check your equations and avoid using global variables inside the function.".format(target.__name__))
-        
+
+
 def multiple_test(test_cases, target):
     success = 0
     for test_case in test_cases:
         try:
-            target_answer = target(*test_case['input'])                   
+            target_answer = target(*test_case['input'])
             if test_case['name'] == "datatype_check":
                 success += datatype_check(test_case['expected'], target_answer, test_case['error'])
             if test_case['name'] == "equation_output_check":
@@ -133,11 +138,11 @@ def multiple_test(test_cases, target):
                 success += shape_check(test_case['expected'], target_answer, test_case['error'])
         except:
             print("Error: " + test_case['error'])
-            
+
     if success == len(test_cases):
         print("\033[92m All tests passed.")
     else:
-        print('\033[92m', success," Tests passed")
+        print('\033[92m', success, " Tests passed")
         print('\033[91m', len(test_cases) - success, " Tests failed")
         raise AssertionError("Not all tests were passed for {}. Check your equations and avoid using global variables inside the function.".format(target.__name__))
-        
+
